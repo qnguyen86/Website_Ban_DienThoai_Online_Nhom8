cf7e00155ed7fd273eac8e4e79a629257bf7d1f6
qnnguyen86 <nhqb612@gmail.com>
2021-10-25 12:29:14 +0700

    Create ModelCategories.php
    model

---
+++ models/ModelCategories.php
@@ -0,0 +1,23 @@
+<?php 
+	trait ModelCategories{
+		public function modelListCategoriesSub($category_id){
+			//lay bien ket noi csdl
+			$conn = Connection::getInstance();
+			//thuc hien truy van
+			$query = $conn->query("select * from categories where parent_id=$category_id");
+			return $query->fetchAll();
+		}
+		public function modelListCategories(){
+			//lay bien ket noi csdl
+			$conn = Connection::getInstance();
+			//thuc hien truy van
+			$query = $conn->query("select * from categories where parent_id = 0 order by id desc");
+			return $query->fetchAll();
+		}
+		public function modelHotNews(){
+			$conn = Connection::getInstance();
+			$query = $conn->query("select * from news where hot=1 order by id desc limit 0,2");
+			return $query->fetchAll();
+		}
+	}
+ ?>
